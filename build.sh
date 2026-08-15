52fd40a02b90e719199f233af06c483dc72729fd
Marius Guščius <marius.guscius@mailbox.org>
2026-08-15 23:00:34 +0300

    local meps file

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 cd src
+
+if [[ ! -f data/meps.xml ]] || (( $(date +%s) - $(stat -c %Y data/meps.xml) > $((24 * 60 * 60)) )); then
+  wget -qO data/meps.xml https://www.europarl.europa.eu/meps/en/full-list/xml
+fi
+
 hugo build --minify --gc
--- serve.sh
+++ serve.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 cd src
-hugo server -D -b http://localhost:1313/ -M --disableFastRender
+
+if [[ ! -f data/meps.xml ]] || (( $(date +%s) - $(stat -c %Y data/meps.xml) > $((24 * 60 * 60)) )); then
+  wget -qO data/meps.xml https://www.europarl.europa.eu/meps/en/full-list/xml
+fi
+
+hugo server -D -b http://localhost:1313/ -M
